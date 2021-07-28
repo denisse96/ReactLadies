ce5b49fcd244e099c2a78b8e20c17564c099fa53
denisse96 <denisse.damian@wizeline.com>
2021-07-28 11:34:42 -0500

    CV

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,50 @@
 import * as React from "react"
 import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
-
+import { Grommet, Box, Text } from "grommet"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
 const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
-  </Layout>
+  <Grommet>
+    <Layout>
+      <Seo title="Home" />
+      <Box pad="medium" background="blue" animation="pulse">
+        Hi React Ladies
+      </Box>
+      <Box pad="medium" background="gray">
+        <Text>
+          I develop scalable, elegant infrastructure as code using Terraform in
+          order to manage and automate multi-platform cloud services to obtain a
+          􏰀ero downtime I ​create Continuous Integration and Continuous
+          Deplo􏰁ment pipelines using Jenkins, GitHub actions as well as Docker
+          containers and Kubernetes to optimi􏰀e team collaboration.
+        </Text>
+      </Box>
+      <Box>
+        <Text>
+          Developed the front-end of an energ􏰁 consumption dashboard using React
+          to monitor and reduce energ􏰁 consumption at manufacturing plants. 􏰊
+          Implemented Mixpanel software to track user data and create A/B
+          testing to anal􏰁􏰀e most frequentl􏰁 used features in energ􏰁 dashboard.
+        </Text>
+      </Box>
+
+      <StaticImage
+        src="../images/gatsby-astronaut.png"
+        width={300}
+        quality={95}
+        formats={["AUTO", "WEBP", "AVIF"]}
+        alt="A Gatsby astronaut"
+        style={{ marginBottom: `1.45rem` }}
+      />
+      <p>
+        <Link to="/page-2/">Go to page 2</Link> <br />
+        <Link to="/page-3/">Go to page 3</Link> <br />
+        <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+      </p>
+    </Layout>
+  </Grommet>
 )
 
 export default IndexPage
